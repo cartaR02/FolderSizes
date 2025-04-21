82174fc121c49d0fefd6e97291f7df7c06c4e0ab
Carter Struck <carterstruck02@gmail.com>
2025-04-21 15:58:51 -0400

    added better naming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FolderSizes",  # Name of your package
-    version="0.1.0",  # Version number
+    version="0.1.2",  # Version number
     packages=find_packages(),  # Find all packages in the folder
     install_requires=[  # List of dependencies, e.g., click
         "click",
@@ -12,12 +12,13 @@ setup(
             'foldersizes = foldersizes.filesizze:dirsizes',  # This defines the CLI command
         ],
     },
+    license="MIT",  # SPDX license expression
     description="A simple script to list folder sizes",  # Short description
     long_description=open('README.md').read(),  # Read the README file for a detailed description
     long_description_content_type='text/markdown',  # File type for long description
-    author="Your Name",
-    author_email="your.email@example.com",  # Replace with your email
-    url="https://github.com/yourusername/foldersizes",  # URL to the project (typically GitHub)
+    author="Carter Struck",
+    author_email="carterstruckm@gmail.com",  # Replace with your email
+    url="https://github.com/cartaR02/FolderSizes",  # URL to the project (typically GitHub)
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # Example license
